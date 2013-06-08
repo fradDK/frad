d4e6b28ce84660cd4d373ddc22f5e86e5f5e6e93
Kwadwo Adu <k@adu.dk>
2013-06-08 11:44:08 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Frad::Application.config.secret_key_base = '9cd843336208560a3fcc4ee9f59a19934ae15618280430fd5700c924cef70ecdaa109c7a3bf73c405ac1bcb67c814c56f90ef2a4664d179437ed766b6deed4bf'
